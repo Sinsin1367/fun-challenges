dc1867770e9224d6ea1eca27a81c87e587879cb3
Sina <sina.asadallahi@gmail.com>
2017-08-19 15:36:40 -0500

    bit test commit

--- bit_test01.py
+++ bit_test01.py
@@ -1,15 +1,15 @@
 import unittest
+from bit import BIT
 
 class test_01(unittest.TestCase):
     arr = [3, 2, -1, 6, 5, 4, -3, 3, 7, 2, 3]
     exp_bit = [0, 3, 5, -1, 10, 5, 9, -3, 19, 7, 9, 3]
     bit_arr = [0] * len(arr)
-    bit = BIT()
-    def test_insert(self):
-        for ind in range(len(arr)):
-            bit.insert(ind, arr[ind])
+    bit = BIT(arr)
 
-        self.assertEqual(bit, exp_bit)
+    def test_insert(self):
+        self.assertEqual(self.bit._arr, self.exp_bit)
+
 
 if __name__ == '__main__':
     unittest.main()
